7473643e90519389f5c9b8fef7add9e37840a3e6
Vikash Chauhan <vikashchauhan51@gmail.com>
2024-05-26 16:18:49 +0530

    added CQRS abstraction

---
+++ src/Ecart.Core/CQRS/ICommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace Ecart.Core.CQRS;
+
+
+/// <summary>
+/// Execute ICommand.
+/// </summary>
+public interface ICommand : ICommand<Unit>
+{
+}
+
+
+/// <summary>
+/// Execute ICommand.
+/// </summary>
+public interface ICommand<out TResponse> : IRequest<TResponse>
+{
+}
