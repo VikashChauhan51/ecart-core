3561d751a2fabd7ce7745dec77f5939fe305924a
Vikash Chauhan <vikashchauhan51@gmail.com>
2024-06-02 12:16:58 +0530

    New: Added new behaviors (#8)

---
+++ src/Ecart.Core/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Ecart.Core.Exceptions;
+public class NotFoundException : Exception
+{
+    public NotFoundException(string message) : base(message)
+    {
+    }
+
+    public NotFoundException(string name, object key) : base($"Entity \"{name}\" ({key}) was not found.")
+    {
+    }
+}
