0443f924df2b22e98028920885963e8d6c63c45f
Vikash Chauhan <vikashchauhan51@gmail.com>
2024-06-15 15:41:37 +0530

    Fix: Updated configuration (#12)

---
+++ src/Ecart.Core/DependencyInjection.cs
@@ -0,0 +1,35 @@
+﻿using Ecart.Core.Configurations;
+using Ecart.Core.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecart.Core;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDaprServices
+        (this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped(typeof(IContentStore<>), typeof(ContentStore<>));
+        services.AddScoped<ISecretStore, SecretStore>();
+        return services;
+    }
+    public static IServiceCollection AddDaprConfiguration
+        (this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<DaprConfig>(configuration.GetSection("Dapr"));
+        return services;
+    }
+
+    public static IServiceCollection AddSqlConfiguration
+        (this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<SqlConfig>(configuration.GetSection("Database"));
+        return services;
+    }
+}
