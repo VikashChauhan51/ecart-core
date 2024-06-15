97cf46b7f64777ad9b717cd8b67baf5fdba3e6bf
Vikash Chauhan <vikashchauhan51@gmail.com>
2024-06-15 15:38:12 +0530

    Fix: Updated configuration

--- src/Ecart.Core/Configurations/SqlConfig.cs
+++ src/Ecart.Core/Configurations/SqlConfig.cs
@@ -3,7 +3,8 @@ namespace Ecart.Core.Configurations;
 public class SqlConfig
 {
     public string ServerName { get; init; } = string.Empty;
-    public string DbName { get; init; } = string.Empty;
+    public int ServerPort { get; init; }
+    public string DatabaseName { get; init; } = string.Empty;
     public string Credentials { get; init; } = string.Empty;
 
 }
