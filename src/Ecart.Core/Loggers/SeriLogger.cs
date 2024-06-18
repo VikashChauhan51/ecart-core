aee43880e0325b7ae70b8190f03aea3ef1d80d1a
Vikash Chauhan <vikashchauhan51@gmail.com>
2024-06-18 20:25:29 +0530

    New: Added SeriLogger

---
+++ src/Ecart.Core/Loggers/SeriLogger.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecart.Core.Loggers;
+public static class SeriLogger
+{
+    public static Action<HostBuilderContext, LoggerConfiguration> Configure =>
+       (context, configuration) =>
+       {
+
+           configuration
+                .Enrich.FromLogContext()
+                .Enrich.WithMachineName()
+                .Enrich.With(new ThreadIdEnricher())
+                .WriteTo.Debug()
+                .WriteTo.Console(outputTemplate: "{Timestamp:HH:mm} [{Level}] ({ThreadId}) {Message}{NewLine}{Exception}")
+                .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
+                .Enrich.WithProperty("Application", context.HostingEnvironment.ApplicationName)
+                .ReadFrom.Configuration(context.Configuration);
+       };
+}
---
+++ src/Ecart.Core/Loggers/ThreadIdEnricher.cs
@@ -0,0 +1,17 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecart.Core.Loggers;
+internal class ThreadIdEnricher : ILogEventEnricher
+{
+    public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+    {
+        logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(
+                "ThreadId", Thread.CurrentThread.ManagedThreadId));
+    }
+}
