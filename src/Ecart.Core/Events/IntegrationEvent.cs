9ab26a0b5c9b621ba63bc12c3df5efe57de12958
Vikash Chauhan <vikashchauhan51@gmail.com>
2024-07-06 13:24:03 +0530

    New: Added pubsub service

--- src/Ecart.Core/Events/IntegrationEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿using Ecart.Core.Domain;
-
-namespace Ecart.Core.Events;
-public record IntegrationEvent: IDomainEvent
-{
-    public Guid Id => Guid.NewGuid();
-    public DateTime OccurredOn => DateTime.Now;
-    public string? EventType => GetType()?.AssemblyQualifiedName;
-}
---
+++ src/Ecart.Core/Services/IEventPublisher.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecart.Core.Services;
+public interface IEventPublisher<in T>
+{
+    Task PublishEventAsync(string topicName, T eventData, CancellationToken token = default);
+}
