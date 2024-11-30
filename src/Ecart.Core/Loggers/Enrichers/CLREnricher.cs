4e247e321ce09e7235043d95c4aa8c37b2cad4b6
Vikash Chauhan <vikashchauhan51@gmail.com>
2024-11-30 23:22:00 +0530

    Fix: removed cqrs from infra core

---
+++ src/Ecart.Core/Loggers/Enrichers/CLREnricher.cs
@@ -0,0 +1,12 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+
+namespace Ecart.Core.Loggers.Enrichers;
+public class CLREnricher : ILogEventEnricher
+{
+    public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+    {
+        logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(
+               "CLRVersion", Environment.Version));
+    }
+}
---
+++ src/Ecart.Core/Loggers/Enrichers/MachineEnricher.cs
@@ -0,0 +1,15 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+
+namespace Ecart.Core.Loggers.Enrichers;
+public class MachineEnricher : ILogEventEnricher
+{
+    public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+    {
+        logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(
+                 "MachineName", Environment.MachineName));
+
+        logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(
+                 "OSVersion", Environment.OSVersion));
+    }
+}
